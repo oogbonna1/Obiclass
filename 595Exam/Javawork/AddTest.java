8a86f5b0ad2b63a9ec91b783a9bb8a4167403f26
oogbonna1 <ogbon@Obi>
2022-04-06 09:05:03 -0700

    First Commit

---
+++ 595Exam/Javawork/AddTest.java
@@ -0,0 +1,16 @@
+package Javawork;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddTest {
+
+	@Test
+	public void Addtest() {
+		MyJunitClass junit = new MyJunitClass();
+		int result = junit.add(100, 200);
+		assertEquals(300, result);
+	}
+
+}
---
+++ 595Exam/Javawork/MyJunitClass.java
@@ -0,0 +1,12 @@
+package Javawork;
+
+public class MyJunitClass {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public String ConCat (String a, String b)
+	{ 
+		return a+b;
+	}
+}
